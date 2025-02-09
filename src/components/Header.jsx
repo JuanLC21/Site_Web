40ed7496a5eefe690623e665a934196a5de663cb
Juan Sebastian <jsebastianlc@gmail.com>
2025-02-09 18:07:24 -0500

    header mobile version, done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,13 @@ import './Header.css'
 export function Header () {
     return (
         <header className='header'>
-            <img className='header__logo' src='./LOGOS DDI.avif'></img>
-            <h1 className='header__title'>Desarrollo De Ingeniería</h1>
-            <div className='header__menu'><Menu/></div>
+            <div className='header__covert'>
+                <nav className="header__naver">
+                    <img className='header__logo' src='./LOGOS DDI.avif' alt='Logo desarrollo de ingeniería'></img>
+                    <div className='header__menu'><Menu/></div>
+                </nav>
+                <h1 className='header__title'>Desarrollo De Ingeniería</h1>
+            </div>
         </header>
     )
 }
