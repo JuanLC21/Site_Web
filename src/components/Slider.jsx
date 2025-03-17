2d4d54d92aedd48164f1f821915b64fcc88b6435
Juan Sebastian <jsebastianlc@gmail.com>
2025-03-16 23:43:30 -0500

    slider mobile version, has been modified

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,37 +1,80 @@
 /* eslint-disable react/prop-types */
-import * as products from '../assets/images/dataImagesA.js'
-import { useEffect, useRef, useState } from 'react'
-import { LeftArrow, RightArrow } from './Icon'
+import { useEffect, useRef, useReducer } from 'react'
+import { LeftArrow, RightArrow, Close } from './Icon'
 import './Slider.css'
 
-const productsImages = Object.values(products)
+const initialState = {
+    position: 0,
+    lightbox: 'slider__lightbox--hide',
+    slider: '',
+    none: ''
+}
 
-export function Slider () {
-    const [indexImage, setIndexImage] = useState(0)
+function reducer (state, action) {
+    switch (action.type) {
+    case 'previous':
+        return { ...state, position: state.position > 0 ? state.position - 1 : action.payload.length - 1 }
+
+    case 'next':
+        return { ...state, position: state.position < action.payload.length - 1 ? state.position + 1 : 0 }
+
+    case 'openLightbox':
+        return { ...state, lightbox: state.lightbox === 'slider__lightbox--hide' ? '' : 'slider__lightbox--hide' }
+
+    case 'openSlider':
+        return { ...state, slider: state.slider === '' ? 'slider__slider--lightbox' : '' }
+
+    case 'hideSlider':
+        return { ...state, none: state.none === '' ? 'slider__slider--none' : '' }
+    }
+}
+
+export function Slider ({ images }) {
+    const productsImages = Object.values(images)
+    const [{ position, lightbox, slider, none }, dispatch] = useReducer(reducer, initialState)
     const indexRef = useRef(null)
-    useEffect(() => {
-        indexRef.current = setInterval(() => {
-            setIndexImage(prevIndex => (prevIndex < productsImages.length - 1 ? prevIndex + 1 : 0))
-        }, 5000)
 
+    useEffect(() => {
+        indexRef.current = setInterval(() => { dispatch({ type: 'next', payload: productsImages }) }, 5000)
         return () => { clearInterval(indexRef.current) }
     }, [])
-    function previous () {
-        setIndexImage(prevIndex => (prevIndex > 0 ? prevIndex - 1 : productsImages.length - 1))
+
+    function previous () { dispatch({ type: 'previous', payload: productsImages }) }
+
+    function next () { dispatch({ type: 'next', payload: productsImages }) }
+
+    function handleSlider () {
+        dispatch({ type: 'openLightbox' })
+        dispatch({ type: 'openSlider' })
+        dispatch({ type: 'hideSlider' })
     }
-    function next () {
-        setIndexImage(prevIndex => (prevIndex < productsImages.length - 1 ? prevIndex + 1 : 0))
-    }
+
     return (
         <section className='slider'>
-            <div className='slider__images'>
-                {productsImages.map((img, index) => (
-                    <img key={index} className='slider__product' src={img} style={{ translate: `${-100 * indexImage}%` }} alt={`Maquina ${index}`}/>
-                ))}
+            <div className={`slider__slider ${none}`} onClick={handleSlider}>
+                <div className='slider__images'>
+                    {productsImages.map((img, index) => (
+                        <img key={index} className='slider__product' src={img} style={{ translate: `${-100 * position}%` }} alt={`Maquina ${index}`}/>
+                    ))}
+                </div>
+                <div className='slider__arrows'>
+                    <button className='slider__button' name='left' onClick={previous}><LeftArrow/></button>
+                    <button className='slider__button slider__button--right' name='right' onClick={next}><RightArrow/></button>
+                </div>
             </div>
-            <div className='slider__arrows'>
-                <button className='slider__button' name='left' onClick={previous}><LeftArrow/></button>
-                <button className='slider__button slider__button--right' name='right' onClick={next}><RightArrow/></button>
+            <div className={`slider__lightbox ${lightbox}`}>
+                <button className='slider__close' onClick={handleSlider}><Close/></button>
+                <div className={`slider__slider ${slider}`}>
+                    <div className='slider__images'>
+                        {productsImages.map((img, index) => (
+                            <img key={index} className='slider__product' src={img} style={{ translate: `${-100 * position}%` }} alt={`Maquina ${index}`}/>
+                        ))}
+                    </div>
+                    <div className='slider__arrows'>
+                        <button className='slider__button' name='left' onClick={previous}><LeftArrow/></button>
+                        <button className='slider__button slider__button--right' name='right' onClick={next}><RightArrow/></button>
+                    </div>
+                </div>
             </div>
         </section>
     )
