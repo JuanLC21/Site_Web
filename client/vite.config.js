776d6c65070a71732976545a16b408716ee29747
DDI SAS <jsebastianlc@gmail.com>
2025-07-01 08:12:51 -0500

    vite.config.js change to force auto update render on browser

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    server: {
+        watch: {
+          usePolling: true,
+          interval: 100, // ms
+        },
+        host: true // expone en 0.0.0.0 para acceso externo si es necesario
+      },
 })
