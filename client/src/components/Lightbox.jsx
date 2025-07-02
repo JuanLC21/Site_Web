e4555110dab88b0a3c167e633ea6b128a59d6973
DDI SAS <jsebastianlc@gmail.com>
2025-07-02 07:55:45 -0500

    component MainProduct has been modified

--- client/src/components/Lightbox.jsx
+++ client/src/components/Lightbox.jsx
@@ -3,14 +3,14 @@ import { useState } from 'react'
 import { Close } from './Icon'
 import './Lightbox.css'
 
-export function Lightbox ({ urlImage, altImage }) {
+export function Lightbox ({ urlImage, altImage, styleMod }) {
     const [openImg, setOpenImg] = useState('lightbox__box--close')
-
+const appearance = styleMod || ''
     function handleOpenImg () {
         setOpenImg(openImg === 'lightbox__box--close' ? '' : 'lightbox__box--close')
     }
     return (
-        <section className="lightbox">
+        <section className={`lightbox ${appearance}`}>
             <img className='lightbox__image' src={urlImage} alt={altImage} onClick={handleOpenImg}/>
             <div className={`lightbox__box ${openImg}`}>
                 <div className="lightbox__container lightbox__container--button">
--- client/src/layouts/main/MainProducts.jsx
+++ client/src/layouts/main/MainProducts.jsx
@@ -5,34 +5,34 @@ import imageProduct00 from '../../assets/images/Etiquetadora.jpg'
 import imageProduct01 from '../../assets/images/Tableros.jpg'
 import imageProduct02 from '../../assets/images/Llenadora1.jpg'
 import imageProduct03 from '../../assets/images/Fermentadora1.jpg'
-import '../../styles/main.css'
+import './MainProducts.css'
 
 export function MainProducts () {
     return (
-        <main className='main'>
-            <Section title={'Máquinas etiquetadoras'}>
+        <main className='main-products'>
+            <Section title={'Máquinas etiquetadoras'} styleMod={'section-products-0'}>
                 Equipos de etiquetado, para envases cilíndricos, ovalados, o caras planas, para  aplicación de etiquetas en la parte superior de envases  y cajas, o bien, equipos multi-cabezales sobre diseño para procesos especiales.
                 <Button direction={'./labeller'}>Conoce más aquí</Button>
             </Section>
-            <Lightbox urlImage={imageProduct00} altImage={'Máquina etiquetadora'}/>
+            <Lightbox urlImage={imageProduct00} altImage={'Máquina etiquetadora'} styleMod={'section-products-1'}/>
 
-            <Section title={'Sistemas de control'}>
+            <Section title={'Sistemas de control'} styleMod={'section-products-2'}>
                 En DDI diseñamos y fabricamos tableros de control a medida de su necesidad con los mejores proveedores y los mas altos estandares de calidad, permitiéndole automatizar los procesos de su compañia.
                 <Button direction={'./panel'}>Conoce más aquí</Button>
             </Section>
-            <Lightbox urlImage={imageProduct01} altImage={'Tableros de control'}/>
+            <Lightbox urlImage={imageProduct01} altImage={'Tableros de control'}styleMod={'section-products-3'}/>
 
-            <Section title={'Máquina Llenadora'}>
+            <Section title={'Máquina Llenadora'} styleMod={'section-products-4'}>
                 Diseñamos y fabricamos maquinas llenadoras de diferentes materias primas como cereales, brindando calidad en materiales y eficiencia en procesos.
                 <Button direction={'./dosing'}>Conoce más aquí</Button>
             </Section>
-            <Lightbox urlImage={imageProduct02} altImage={'Máquina dosificadora'}/>
+            <Lightbox urlImage={imageProduct02} altImage={'Máquina dosificadora'} styleMod={'section-products-5'}/>
 
-            <Section title={'Cámara de fermentación'}>
+            <Section title={'Cámara de fermentación'} styleMod={'section-products-6'}>
                 Nuestra cámara de fermentación permite realizar el proceso de fermentación de la masa, al tener control total del nivel de temperatura y humedad en la cámara.
                 <Button direction={'./fermentation'}>Conoce más aquí</Button>
             </Section>
-            <Lightbox urlImage={imageProduct03} altImage={'Cámara de fermentación'}/>
+            <Lightbox urlImage={imageProduct03} altImage={'Cámara de fermentación'} styleMod={'section-products-7'}/>
         </main>
     )
 }
