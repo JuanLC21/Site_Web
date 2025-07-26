47ac1655f94ee43118cdae691a2469a62d112450
Juan Sebastian <jsebastianlc@gmail.com>
2025-07-26 15:28:11 -0500

    useIntersectionObserver hook has been integrated

--- client/src/hooks/useIntersectionObserver.js
+++ client/src/hooks/useIntersectionObserver.js
@@ -3,14 +3,13 @@ import { useEffect, useRef, useState } from 'react'
 export function useIntersectionObserver () {
     const [isObserved, setIsObserved] = useState(false)
     const refElement = useRef()
-
     useEffect(() => {
         const observer = new IntersectionObserver((entries) => {
             setIsObserved(entries[0].isIntersecting)
         }, { threshold: 0.1 })
 
         if (refElement.current) observer.observe(refElement.current)
-        return () => { observer.unobserve(refElement.current) }
+        return () => { if (refElement.current) observer.unobserve(refElement.current) }
     }, [])
 
     return { isObserved, refElement }
